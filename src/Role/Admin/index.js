f9dc3375da1dafe7c4462c9795467923c817d7eb
Muhammad Hammad <hafizhammadmemon38@gmail.com>
2025-08-07 18:17:53 +0500

    some changes

--- src/Role/Admin/index.js
+++ src/Role/Admin/index.js
@@ -1,9 +1,17 @@
 import CreateStaff from "./CreateStaff";
 import AllBeneficiaries from "./AllBeneficariy";
-import BeneficiaryDetail from "./benefiacriyDetail";
+import BeneficiaryDetail from "./BeneficiaryDetail";
 import AllTokens from "./AllToken";
 import NewBeneficiaries from "./Newbeneficairy";
 import Reports from "./Report";
 import VerifyOtpPage from "./otpPage";
 
-export {CreateStaff, AllBeneficiaries,BeneficiaryDetail,AllTokens,NewBeneficiaries, Reports, VerifyOtpPage}
+export {
+  CreateStaff,
+  AllBeneficiaries,
+    BeneficiaryDetail,
+  AllTokens,
+  NewBeneficiaries,
+  Reports,
+  VerifyOtpPage
+};
